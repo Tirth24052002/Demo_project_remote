01eb74378632f35277a33aeb23993e644e45dc6b
Tirth24052002 <tirth.p@simformsolutions.com>
2023-02-14 15:32:45 +0530

    Change app content

---
+++ Demo_project/SignUp_Page.swift
@@ -0,0 +1,29 @@
+//
+//  SignUp_page.swift
+//  Demo_project
+//
+//  Created by Tirth Purohit on 14/02/23.
+//
+
+import UIKit
+
+class SignUp_Page: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
